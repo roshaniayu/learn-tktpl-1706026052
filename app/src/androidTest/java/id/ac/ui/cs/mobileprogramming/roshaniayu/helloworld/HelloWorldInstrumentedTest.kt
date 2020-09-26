b3ae1e87a98cf8c2b67efc913ca4a85dd3a6bee2
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2020-09-26 12:19:00 +0700

    rename and add test for helloworld app

---
+++ app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/roshaniayu/helloworld/HelloWorldInstrumentedTest.kt
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.mobileprogramming.roshaniayu.helloworld
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class HelloWorldInstrumentedTest {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun whenButtonIsNotClickedItIsLightMode() {
+        Espresso.onView(ViewMatchers.withId(R.id.helloText))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")))
+    }
+
+    @Test
+    fun whenButtonIsClickedItIsDarkMode() {
+        Espresso.onView(ViewMatchers.withId(R.id.modeButton)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.helloText))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Goodnight World!")))
+    }
+}
