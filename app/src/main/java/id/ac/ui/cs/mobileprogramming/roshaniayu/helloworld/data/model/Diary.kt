f70a6fd1d484bf633bebf161dcf43872074cbaa8
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2020-10-10 01:51:39 +0700

    change diary model

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/roshaniayu/helloworld/data/model/Diary.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/roshaniayu/helloworld/data/model/Diary.kt
@@ -2,6 +2,6 @@ package id.ac.ui.cs.mobileprogramming.roshaniayu.helloworld.data.model
 
 data class Diary(val feeling: String, val name: String) {
     override fun toString(): String {
-        return "$feeling -$name"
+        return "\"$feeling\" -$name"
     }
 }
